9c60bc82fea48f8d7ffb633263760b8baa38a322
Ballursajeed <ballursajeed@gmail.com>
2024-01-15 19:14:05 +0530

    deployed on vercel

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -25,7 +25,7 @@ const Chat = () => {
  },[]);
 
 const connectToWs = () => { //connecting web socket server
-    const ws = new WebSocket('wss://localhost:8000');
+    const ws = new WebSocket('wss://chat-app-mern-server.vercel.app');
     setWs(ws);
     ws.addEventListener('message',handleMessage);
     ws.addEventListener('close',() => {
